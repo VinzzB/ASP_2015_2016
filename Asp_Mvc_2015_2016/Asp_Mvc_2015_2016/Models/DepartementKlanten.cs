3056d57764b859ae43ab59a56f09a8deb6e18597
VinzzB <vinzz@vinzz.be>
2015-12-12 05:56:42 +0100

    added untracked file 'DepartementKlanten.cs'

---
+++ Asp_Mvc_2015_2016/Asp_Mvc_2015_2016/Models/DepartementKlanten.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asp_Mvc_2015_2016.Models
+{
+    [Table("DepartementKlanten")]
+    public class DepartementKlanten : _BaseInfo
+    {        
+        public virtual Departement Departement { get; set; }
+        public virtual Klant Klant { get; set; }
+    }
+}
