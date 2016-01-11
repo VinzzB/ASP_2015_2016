50842dd3d2a3eff75506b44833461d8c6ec5b2d3
Nathalie23 <natmathieu@gmail.com>
2016-01-11 01:00:53 +0100

    nuget

--- Asp_Mvc_2015_2016/Asp_Mvc_2015_2016/App_Start/BundleConfig.cs
+++ Asp_Mvc_2015_2016/Asp_Mvc_2015_2016/App_Start/BundleConfig.cs
@@ -31,6 +31,32 @@ namespace Asp_Mvc_2015_2016
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            //Added by Nathalie
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            "~/Content/themes/base/accordion.css",
+            "~/Content/themes/base/all.css",
+            "~/Content/themes/base/autocomplete.css",
+            "~/Content/themes/base/base.css",
+            "~/Content/themes/base/button.css",
+            "~/Content/themes/base/core.css",
+            "~/Content/themes/base/datepicker.css",
+            "~/Content/themes/base/dialog.css",
+            "~/Content/themes/base/draggable.css",
+            "~/Content/themes/base/menu.css",
+            "~/Content/themes/base/progressbar.css",
+            "~/Content/themes/base/resizable.css",
+            "~/Content/themes/base/selectable.css",
+            "~/Content/themes/base/selectmenu.css",
+            "~/Content/themes/base/slider.css",
+            "~/Content/themes/base/sortable.css",
+            "~/Content/themes/base/spinner.css",
+            "~/Content/themes/base/tabs.css",
+            "~/Content/themes/base/theme.css",
+            "~/Content/themes/base/tooltip.css"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
